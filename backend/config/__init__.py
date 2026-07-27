bd24785b64b2ba931eb850339c20b7035ee2f052
rezaak7698 <113626592+rezaak7698@users.noreply.github.com>
2026-07-27 07:57:09 +0330

    phase 0: backend - config init

---
+++ backend/config/__init__.py
@@ -0,0 +1,8 @@
+"""Pinzo Django project."""
+
+# This will make sure the app is always imported when
+# Django starts so that shared_task will use this app.
+from .celery import app as celery_app  # noqa: F401
+
+__all__ = ("celery_app",)
+__version__ = "0.1.0"
