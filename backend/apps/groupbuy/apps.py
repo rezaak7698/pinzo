335df4a78377a664166f9f93e9773f5d362ab589
rezaak7698 <113626592+rezaak7698@users.noreply.github.com>
2026-07-28 12:34:21 +0330

    phase 0: groupbuy apps.py

---
+++ backend/apps/groupbuy/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class GroupbuyConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.groupbuy"
+    verbose_name = "Group Buy"
